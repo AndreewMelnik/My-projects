44be1365d42cf9b91f281301c71964d195ecb803
Andrew <89791357+AndreewMelnik@users.noreply.github.com>
2022-02-26 15:57:17 +0300

    rightvariant

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 
 const personalMovieDB = {
-  count: "${personalMovieDB}" ,
-  movies: "${nameOfFilms}, ${ratingOfFilms}",
+  count: numberOfFilms ,
+  movies: {},
   actors: {},
   genres: [],
   private: false,
 };
-const nameOfFilms = prompt("Один из последних просмотренных фильмов?");
+const nameOfFilms = prompt("Один из последних просмотренных фильмов?",""),
+      ratingOfFilms = prompt("Нас сколько оцените его?",""),
+      nameOfFilms2 = prompt("Один из последних просмотренных фильмов?",""),
+      ratingOfFilms2 = prompt("Нас сколько оцените его?","");
 
-const ratingOfFilms = prompt("Нас сколько оцените его?");
+personalMovieDB.movies [nameOfFilms] = ratingOfFilms;    
+personalMovieDB.movies [nameOfFilms2] = ratingOfFilms2;   
+console.log(personalMovieDB);
