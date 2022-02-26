9cf8e5f2b32fc262b2d0ea4eff88ff0385e41a2f
Andrew <89791357+AndreewMelnik@users.noreply.github.com>
2022-02-26 15:44:02 +0300

    my variant

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-123
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+
+
+const personalMovieDB = {
+  count: "${personalMovieDB}" ,
+  movies: "${nameOfFilms}, ${ratingOfFilms}",
+  actors: {},
+  genres: [],
+  private: false,
+};
+const nameOfFilms = prompt("Один из последних просмотренных фильмов?");
+
+const ratingOfFilms = prompt("Нас сколько оцените его?");
