ad3e59821e02e337f1fa3d2128fbffc1018b4a0f
Andrew <89791357+AndreewMelnik@users.noreply.github.com>
2022-03-01 17:03:14 +0300

    final variant

--- script.js
+++ script.js
@@ -24,14 +24,19 @@ for (let i = 0; i < 2; i++) {
       alert('error!');
       i--;
     }
-}
+  if (personalMovieDB.count < 10){
+    console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30){
+      console.log('Вы классический зритель');
+    } else if (personalMovieDB.count > 30){
+        console.log('Вы киноман!');
+    } else {
+        console.log('Произошла ошибка');
+    }
+  }
 
-   
-
-
-
-  
 
 
  
  console.log(personalMovieDB);
+  
