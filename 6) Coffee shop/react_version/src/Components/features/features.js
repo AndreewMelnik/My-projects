f656ce20739b423297a48b2be208bebee36ea770
Andrew <89791357+AndreewMelnik@users.noreply.github.com>
2022-05-12 17:38:56 +0300

    ю

--- 6) Coffee shop/react_version/src/Components/features/features.js
+++ 6) Coffee shop/react_version/src/Components/features/features.js
@@ -10,55 +10,55 @@ const Features = () => {
 
 <div class="coffee__wrap">
         <div class="coffee__text">
-          <h1 class="coffee__subtitle subtitle">Your Personalized Coffee
-          </h1>
-          <h2 class="coffee__title title">COFFEE BUILD YOUR BASE
-          </h2>
+            <h1 class="coffee__subtitle subtitle">Your Personalized Coffee
+            </h1>
+            <h2 class="coffee__title title">COFFEE BUILD YOUR BASE
+            </h2>
         </div>
         <div class="coffee__cards">
-          <div class="coffee__card">
-            <div class="coffee__card-pic">
-              <img src={coffeeShop}  alt="coffeeshop" class="coffeeshop"/>
-            </div>
-            <h3 class="coffee__card-title">
-              Origin
-            </h3>
-            <p class="coffee__card-desc">
-              Arabica coffee beans, robusta quality meets international standards.
-            </p>
+            <div class="coffee__card">
+              <div class="coffee__card-pic">
+                <img src={coffeeShop} alt="coffeeshop" class="coffeeshop"/>
+              </div>
+              <h3 class="coffee__card-title">
+                Origin
+              </h3>
+              <p class="coffee__card-desc">
+                Arabica coffee beans, robusta quality meets international standards.
+              </p>
           </div>
           <div class="coffee__card">
-            <div class="coffee__card-pic">
-              <img src={beans}alt="beans" class="beans"/>
-            </div>
-            <h3 class="coffee__card-title">
-              Quality
-            </h3>
-            <p class="coffee__card-desc">
-              Each coffee bean is a highly concentrated process of coffee artisans.
-            </p>
+              <div class="coffee__card-pic">
+                <img src={beans} alt="beans" class="beans"/>
+              </div>
+              <h3 class="coffee__card-title">
+                Quality
+              </h3>
+              <p class="coffee__card-desc">
+                Each coffee bean is a highly concentrated process of coffee artisans.
+              </p>
           </div>
           <div class="coffee__card">
-            <div class="coffee__card-pic">
-              <img src={cups} alt="cups" class="cups"/>
-            </div>
-            <h3 class="coffee__card-title">
-              Flavour
-            </h3>
-            <p class="coffee__card-desc">
-              70% of the coffee cup flavour comes from the origin and quality of the coffee.
-            </p>
+              <div class="coffee__card-pic">
+                <img src={cups} alt="cups" class="cups"/>
+              </div>
+              <h3 class="coffee__card-title">
+                Flavour
+              </h3>
+              <p class="coffee__card-desc">
+                70% of the coffee cup flavour comes from the origin and quality of the coffee.
+              </p>
           </div>
           <div class="coffee__card">
-            <div class="coffee__card-pic">
-              <img src={coffeeMachine} alt="coffeemachine" class="coffeemachine"/>
-            </div>
-            <h3 class="coffee__card-title">
-              Preparation
-            </h3>
-            <p class="coffee__card-desc">
-             The coffee beans are molted through a thorough and accurate roasting process.
-            </p>
+              <div class="coffee__card-pic">
+                <img src={coffeeMachine} alt="coffeemachine" class="coffeemachine"/>
+              </div>
+              <h3 class="coffee__card-title">
+                Preparation
+              </h3>
+              <p class="coffee__card-desc">
+              The coffee beans are molted through a thorough and accurate roasting process.
+              </p>
           </div>
            
         </div>
--- 6) Coffee shop/react_version/src/Components/giftset/giftset.js
+++ 6) Coffee shop/react_version/src/Components/giftset/giftset.js
@@ -37,11 +37,11 @@ onToggleIncrease = (id) => {
 
 
 const giftsetData = [
-    {name: "GIFTSET “CÀ PHÊ PHIN VIỆT NAM”", price: "$ 20.00", src:giftset1, beans: "Độ cao", height: "700 - 800m ",id:1, 
+    {name: "GIFTSET “CÀ PHÊ PHIN VIỆT NAM”", price: "$ 20.00", src:giftset1, beans: "Độ cao", height: "700 - 800m ",id: 1, 
     description: "Món quà tuyệt vời dành cho người sành cà phê.Hạt cà phê được Revo cẩn trọng chọn lựa, rang xay theo công nghệ và bí quyết học hỏi từ nhữngnghệ nhân nổi tiếng, cùng với tình yêu, sự đam mê của người làm cà phê… tạo ra những táchcà phê tinh khiết chỉ dành riêng cho bạn."},
-    {name: "GIFTSET “TỐ NỮ", price: "$ 23.00", src:giftset2, beans: "Độ cao", height: "700 - 800m ", id:2, 
+    {name: "GIFTSET “TỐ NỮ", price: "$ 23.00", src:giftset2, beans: "Độ cao", height: "700 - 800m ", id: 2, 
     description: "Món quà tặng rất Việt Nam: thể hiện tính truyền thống từ sản phẩm cho tới bao bì và được đặt trong thiết kế hiện đại – độc đáo!.Bộ giftset Tố Nữ tiêu chuẩn bao gồm: – Hộp đựng 2 tầng bằng giấy kraft, với thiết kế lấy ý tưởng từ bộ tranh Tố Nữ – thuộc dòng tranh dân gian Hàng Trống– Gói cà phê Revo Morning 250gr – với gu vị cân bằng, là món quà tuyệt vời dành cho những người nước ngoài muốn thử hương vị cà phê Việt Nam chất lượng cao– Phin mạ màu cao cấp từ Revo Coffee"},
-    {name: "MODERN VIETNAMESE COFFEE BLEND BOX", price: "$ 25.00",src:giftset3,beans: "Độ cao", height: "700 - 800m ", id:3, 
+    {name: "MODERN VIETNAMESE COFFEE BLEND BOX", price: "$ 25.00",src:giftset3,beans: "Độ cao", height: "700 - 800m ", id: 3, 
     description: "Trên các cao nguyên của Việt Nam.Cây cà phê được chăm chút bởi hơi nước , ánh sáng , gió , mưa  và sự cần cù của người nông dân chân chất, kết tinh thành những hạt cà phê chất lượng, đậm đà, đặc trưng…Hạt cà phê được Revo cẩn trọng chọn lựa, rang xay theo công nghệ và bí quyết học hỏi từ những nghệ nhân nổi tiếng, cùng với tình yêu, sự đam mê của người làm cà phê… tạo ra những tách cà phê tinh khiết chỉ dành riêng cho bạn.Bộ blend box gồm 3 hương vị đặc trưng của Revo: Revo Origin, Revo Natural, Revo Honey."},
 ]
 
