a332c524035a389fd853af743b438133e79a7105
Andrew <89791357+AndreewMelnik@users.noreply.github.com>
2022-05-18 15:09:37 +0300

    Список с вопросами

--- 8) React newApp/src/Components/ActiveQuiz/ActiveQuiz.js
+++ 8) React newApp/src/Components/ActiveQuiz/ActiveQuiz.js
@@ -1,9 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './ActiveQuiz.css';
+import AnswersList from '../AnswersList/AnswersList';
 
-const ActiveQuiz = (props) => {
-  <div>
-      
+const ActiveQuiz = props => (
+  <div className="ActiveQuiz">
+   <p>
+       <span>
+           <strong>2.</strong>&nbsp;
+           How are you?
+       </span>
+
+       <small>4 из 12</small>
+   </p>
+   <AnswersList
+     answers={props.answers}
+   />
   </div>
 
-}
+)
+export default ActiveQuiz
